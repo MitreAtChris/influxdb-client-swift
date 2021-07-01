c50aceef6f82c725a66ad6f363e2ab0a84db3383
MitreAtChris <86261388+MitreAtChris@users.noreply.github.com>
2021-07-01 10:37:49 -0400

    Update InfluxDBClient.swift

--- Sources/InfluxDBSwift/InfluxDBClient.swift
+++ Sources/InfluxDBSwift/InfluxDBClient.swift
@@ -32,7 +32,7 @@ public class InfluxDBClient {
     /// The options to configre client.
     internal let options: InfluxDBOptions
     /// Shared URLSession across the client.
-    public lazy let session: URLSession
+    public lazy var session: URLSession
 
     /// Lazy initialized `QueryAPI`.
     public lazy var queryAPI: QueryAPI = { QueryAPI(client: self) }()
