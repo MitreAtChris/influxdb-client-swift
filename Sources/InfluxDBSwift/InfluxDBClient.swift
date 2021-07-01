1bb02768696dc89a307b7d502a743d9cf551650a
MitreAtChris <86261388+MitreAtChris@users.noreply.github.com>
2021-07-01 10:18:59 -0400

    Update InfluxDBClient.swift

--- Sources/InfluxDBSwift/InfluxDBClient.swift
+++ Sources/InfluxDBSwift/InfluxDBClient.swift
@@ -65,7 +65,7 @@ public class InfluxDBClient {
         configuration.timeoutIntervalForResource = self.options.timeoutIntervalForResource
         configuration.protocolClasses = protocolClasses
 
-        session = URLSessionDelegate(configuration: configuration, delegate: self, delegateQueue: nil)
+        session = URLSession(configuration: sessionConfiguration(), delegate: self, delegateQueue: nil)
     }
 
     /// Create a new client for InfluxDB 1.8 compatibility API.
@@ -105,10 +105,11 @@ public class InfluxDBClient {
     public func close() {
         session.invalidateAndCancel()
     }
-    func URLSessionDelegate(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-        let urlCredential = URLCredential(trust: challenge.protectionSpace.serverTrust!)
-        completionHandler(.useCredential, urlCredential)
-}
+    private func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: (URLSession.AuthChallengeDisposition) -> Void) {
+            completionHandler(
+                .cancelAuthenticationChallenge
+            )
+        }
 }
 
 extension InfluxDBClient {
